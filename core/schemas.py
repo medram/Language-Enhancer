4ce0aff6e35f93980229f2a73de506ec401b4c61
medram <mramouchy@gmail.com>
2024-01-21 20:04:13 +0100

    Fixed OpenAIModel schema

--- core/schemas.py
+++ core/schemas.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 class OpenAIModel(BaseModel):
     name: str
     max_tokens: int = 4096
-    is_chat_model: Optional[bool] = False
+    is_chat_model: bool = False
 
     def __str__(self) -> str:
         return self.name
