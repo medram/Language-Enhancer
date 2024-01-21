38b57ec5ed60b4ac55a231e5ebb40ace66afb1b3
medram <mramouchy@gmail.com>
2024-01-21 15:10:40 +0100

    Page 2 added

--- pages/page_2.py
+++ pages/page_2.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
-st.write("# Page 2")
+from core.sidebar import sidebar
+
+# Sidebar
+sidebar()
+
+
+st.text_area("**My Text:**")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    enhance = st.button("🔁 Enhance My Text!", use_container_width=True)
+with col2:
+    my_level = st.button("✨ Detect My Current Text Level?", use_container_width=True)
+
+if enhance:
+    st.write(f"### Enhanced Text (Level: {st.session_state.english_level}):")
+
+if my_level:
+    st.write("### My Current Text Level:")
