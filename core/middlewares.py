12e8a225acfb55a83ae1e54e4d1c28c3eed7e8da
medram <mramouchy@gmail.com>
2024-01-21 18:57:57 +0100

    Fixed sidebar & middleware

--- core/middlewares.py
+++ core/middlewares.py
@@ -1,8 +1,7 @@
 import streamlit as st
-
 from streamlit_extras.switch_page_button import switch_page
 
 
 def openai_required_middleware():
-	if "openai_key" not in st.session_state:
-		switch_page("OpenAI key")
+    if "openai_key" not in st.session_state:
+        switch_page("app")
