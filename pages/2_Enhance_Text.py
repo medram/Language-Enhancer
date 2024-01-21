321782931c099ea417d4c061557cb2dcd1881641
medram <mramouchy@gmail.com>
2024-01-21 20:02:25 +0100

    Enhanced Text Page added

--- pages/2_Enhance_Text.py
+++ pages/2_Enhance_Text.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 from core.middlewares import openai_required_middleware
 from core.ui.sidebar import sidebar
+from core.utils import get_enhanced_text, get_my_current_text_level
 
 # A Middleware-like for checking OpenAI key
 openai_required_middleware()
@@ -11,7 +12,7 @@ sidebar()
 
 
 st.write("# Enhance Text")
-st.text_area("**My Text:**")
+text: str = st.text_area("**My Text:**")
 
 
 col1, col2 = st.columns(2)
@@ -23,6 +24,9 @@ with col2:
 
 if enhance:
     st.write(f"### Enhanced Text (Level: {st.session_state.english_level}):")
+    st.write(get_enhanced_text(text))
+
 
 if my_level:
     st.write("### My Current Text Level:")
+    st.write(get_my_current_text_level(text))
