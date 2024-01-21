53ffb54aa8f7057c90cf63b1ad54f5f7c057a8af
medram <mramouchy@gmail.com>
2024-01-21 20:13:22 +0100

    Spinner added during using OpenAI

--- pages/2_Enhance_Text.py
+++ pages/2_Enhance_Text.py
@@ -19,14 +19,15 @@ col1, col2 = st.columns(2)
 
 with col1:
     enhance = st.button("🔁 Enhance My Text!", use_container_width=True)
+
 with col2:
     my_level = st.button("✨ Detect My Current Text Level?", use_container_width=True)
 
-if enhance:
-    st.write(f"### Enhanced Text (Level: {st.session_state.english_level}):")
-    st.write(get_enhanced_text(text))
+with st.spinner("Running..."):
+    if enhance:
+        st.write(f"### Enhanced Text (Level: {st.session_state.english_level}):")
+        st.write(get_enhanced_text(text))
 
-
-if my_level:
-    st.write("### My Current Text Level:")
-    st.write(get_my_current_text_level(text))
+    if my_level:
+        st.write("### My Current Text Level:")
+        st.write(get_my_current_text_level(text))
